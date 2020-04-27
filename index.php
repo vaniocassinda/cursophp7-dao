252d8eba202d3a1442e9491e3843bca9f8200fca
Vanio Cassinda <vanilsonvanio23@gmail.com>
2020-04-27 20:02:19 +0100

    Metodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,25 @@
 
 require_once("config.php");
 
-	$jose = new Usuario();
+	//Carrega um usuário
+	//$jose = new Usuário();
+	//$jose->LoadById(3);
+	//echo $jose;
 
-	$jose->LoadById(3);
+	//Carregga uma lista de usuários
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+	
+	//Carrega uma lista de usuários buscando pelo login
+	
+	//$search = Usuario::search("j");
+	//echo json_encode($search);
+	
+	//Carrega um usuário usando o login e a senha
 
-	echo $jose;
+	$usuario = new Usuario();
+	$usuario->login("jose","123456");
+
+	echo ($usuario);
+
  ?>
